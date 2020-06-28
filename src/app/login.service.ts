d6664092c25ec8caf285794b32619c9435f71271
Divyatej <vikash.bitcs@gmail.com>
2020-06-29 00:02:25 +0530

    Changes in front end codes

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const httpOptions={
-  headers:new HttpHeaders({'Content-Type': 'application/json'})
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  private Login_url="http://localhost:8080/onlinegaming/"
+  private Login_url = "http://localhost:5050/onlinegaming/login"
 
-  constructor(private httpClient:HttpClient) { }
-  public getLogin(login): Observable<any>{
-    return this.httpClient.get<any>(this.Login_url+"login", login);
+  constructor(private httpClient: HttpClient) { }
+  
+  public getLogin(login): Observable<any> {
+    return this.httpClient.post<any>(this.Login_url + "/getLogin", login,httpOptions);
   }
+
 }
