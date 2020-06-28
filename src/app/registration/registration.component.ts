2ab6fbbc46bb015dd925108389e546ef3f4d488f
Dileepkvh <k.v.h.dileep@outlook.com>
2020-06-29 02:16:15 +0530

    Added registration.service.ts and registration.ts Modified registration.component.html and registration.component.ts

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Registration } from '../registration';
+import { RegistrationService } from '../registration.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registration:Registration=new Registration();
+  response:any
+  fullName:string;
+	fatherName:string;
+	email:string;
+	gender:string;
+	dob:string;
+	pinCode:string;
+	address:string;
+	village:string;
+	city:string;
+	mobileNumber:string;
+	password:string;
+
+  constructor( private registrationServise:RegistrationService) {
+  }
 
   ngOnInit() {
+  }
+
+  
+  public regdtls() {
+    this.registration.fullName = this.fullName;
+    this.registration.fatherName = this.fatherName;
+    this.registration.email = this.email;
+    this.registration.gender=this.gender;
+    this.registration.dob= this.dob;
+    this.registration.pinCode=this.pinCode;
+    this.registration.address=this.address;
+    this.registration.village=this.village;
+    this.registration.city=this.city;
+    this.registration.mobileNumber=this.mobileNumber;
+    this.registration.password=this.password;
+    console.log(this.registration);
+    this.registrationServise.regdtls(this.registration).subscribe(
+      data=> {
+        this.response = data; 
+        console.log(data) 
+      },
+      error => console.log(error));
   }
 
 }
