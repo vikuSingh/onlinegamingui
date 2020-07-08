b11838c8d892384f39087ebcb24fe466ece46e70
vikas <vkumar@loginsoft.com>
2020-07-08 06:08:26 +0530

    registration service changes.

--- src/app/registration.service.ts
+++ src/app/registration.service.ts
@@ -3,9 +3,7 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +14,7 @@ export class RegistrationService {
   constructor(private httpClient : HttpClient) { }
 
   public saveRegistration(registration) : Observable<any> {
-    return this.httpClient.post<any>(this.REG_URL+"/save",registration,httpOptions);
+    return this.httpClient.post<any>(this.REG_URL+"/save",registration,{headers,responseType: 'text' as 'json' });
   }
 
   public getRegistration() : Observable<any> {
